44acb9911a3eb472567bc225f5464f39b4701e19
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-11-28 23:33:15 +0000

    Added 2nd api request and load event. #1

--- js/data.js
+++ js/data.js
@@ -1,1 +1,15 @@
 /* exported data */
+let pokemon = {
+  view: 'pokedex',
+  entries: [],
+};
+
+window.addEventListener('beforeunload', (event) => {
+  const pokemonJSON = JSON.stringify(pokemon);
+  localStorage.setItem('ajax-project', pokemonJSON);
+});
+
+const previousPokemonJSON = localStorage.getItem('ajax-project');
+if (previousPokemonJSON !== null) {
+  pokemon = JSON.parse(previousPokemonJSON);
+}
--- js/pokemon.js
+++ js/pokemon.js
@@ -1,21 +1,33 @@
-function getPokemonData(pokemon) {
+function getPokemonData(name) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/');
   xhr.responseType = 'json';
 
   xhr.addEventListener('load', function () {
-    console.log(xhr.status);
     console.log(xhr.response);
     const genOne = xhr.response;
 
-    for (let i = 0; i < genOne.length; i++) {
-      const $pokemon = genOne[i];
-      const $row = document.querySelector('.row');
-      $row.appendChild($pokemon);
-    }
+    const xhr2 = new XMLHttpRequest();
+    xhr2.open('GET', 'https://pokeapi.co/api/v2/pokemon/1/');
+    xhr2.responseType = 'json';
+
+    xhr2.addEventListener('load', function () {
+      const $rowPokemon = document.querySelector('.row-pokemon');
+      console.log(xhr2.response);
+
+      for (let i = 0; i < xhr2.response.length; i++) {
+        const $pokemon = renderPokemon(xhr2.response[i]);
+        $rowPokemon.appendChild($pokemon);
+      }
+    });
+    xhr2.send();
   });
   xhr.send();
+}
 
+getPokemonData('bulbasaur');
+
+function renderPokemon(pokemon) {
   const $colDiv = document.createElement('div');
   $colDiv.setAttribute('class', 'column-one-fifth');
 
@@ -23,7 +35,7 @@ function getPokemonData(pokemon) {
   $pcDiv.setAttribute('class', 'pokemon-card');
 
   const $pokeImage = document.createElement('image');
-  $pokeImage.setAttribute('src', pokemon.imageURl);
+  $pokeImage.setAttribute('src', pokemon.sprites.front_default);
 
   const $pokeText = document.createElement('div');
   $pokeText.setAttribute('class', 'pokemon-card-text');
@@ -32,19 +44,17 @@ function getPokemonData(pokemon) {
   $name.textContent = pokemon.name;
 
   const $number = document.createElement('h3');
-  $number.textContent = '#' + pokemon.number;
+  $number.textContent = '#' + pokemon.id;
 
-  const $description = document.createElement('p');
-  $description.textContent = pokemon.description;
+  // const $description = document.createElement('p');
+  // $description.textContent = pokemon.description;
 
   $colDiv.appendChild($pcDiv);
   $pcDiv.appendChild($pokeImage);
   $pcDiv.appendChild($pokeText);
   $pokeText.appendChild($name);
   $pokeText.appendChild($number);
-  $pokeText.appendChild($description);
+  // $pokeText.appendChild($description);
 
   return $colDiv;
 }
-
-getPokemonData('bulbasaur');
