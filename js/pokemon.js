08afe9cc7a0338a5b86f77b83db858e332d3b362
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-11-29 18:56:46 +0000

    Added media styles and CSS styling for card display. #1

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,26 +1,23 @@
 function getPokemonData(name) {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/');
+  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon?limit=151/');
   xhr.responseType = 'json';
 
   xhr.addEventListener('load', function () {
     console.log(xhr.response);
     const genOne = xhr.response;
-
-    const xhr2 = new XMLHttpRequest();
-    xhr2.open('GET', 'https://pokeapi.co/api/v2/pokemon/1/');
-    xhr2.responseType = 'json';
-
-    xhr2.addEventListener('load', function () {
-      const $rowPokemon = document.querySelector('.row-pokemon');
-      console.log(xhr2.response);
-
-      for (let i = 0; i < xhr2.response.length; i++) {
-        const $pokemon = renderPokemon(xhr2.response[i]);
-        $rowPokemon.appendChild($pokemon);
-      }
-    });
-    xhr2.send();
+    console.log('xhr', genOne.results);
+    for (let i = 0; i < genOne.results.length; i++) {
+      const xhr2 = new XMLHttpRequest();
+      xhr2.open('GET', `${genOne.results[i].url}`);
+      xhr2.responseType = 'json';
+      xhr2.addEventListener('load', function () {
+        console.log('xh2', xhr2.response);
+        const $rowPokemon = document.querySelector('.row-pokemon');
+        $rowPokemon.appendChild(renderPokemon(xhr2.response));
+      });
+      xhr2.send();
+    }
   });
   xhr.send();
 }
@@ -34,7 +31,7 @@ function renderPokemon(pokemon) {
   const $pcDiv = document.createElement('div');
   $pcDiv.setAttribute('class', 'pokemon-card');
 
-  const $pokeImage = document.createElement('image');
+  const $pokeImage = document.createElement('img');
   $pokeImage.setAttribute('src', pokemon.sprites.front_default);
 
   const $pokeText = document.createElement('div');
@@ -46,15 +43,15 @@ function renderPokemon(pokemon) {
   const $number = document.createElement('h3');
   $number.textContent = '#' + pokemon.id;
 
-  // const $description = document.createElement('p');
-  // $description.textContent = pokemon.description;
+  const $experience = document.createElement('p');
+  $experience.textContent = pokemon.base_experience;
 
   $colDiv.appendChild($pcDiv);
   $pcDiv.appendChild($pokeImage);
   $pcDiv.appendChild($pokeText);
   $pokeText.appendChild($name);
   $pokeText.appendChild($number);
-  // $pokeText.appendChild($description);
+  $pokeText.appendChild($experience);
 
   return $colDiv;
 }
